b2443cdb6e54b5555021d49ba2bb137e063de3d0
jwkim <phoongdeng@gmail.com>
2020-05-25 03:06:01 +0900

    initialize project - hellp world

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import { unregister } from './core'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<div>Hello World</div>, document.getElementById('root'))
 
 unregister()
