219ede4182439d60ddd1163cdb5b21cd598191bb
jwkim <phoongdeng@gmail.com>
2020-05-26 01:28:44 +0900

    apply card component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
-import { Content, Title } from './components'
+import { Card, Content, Title } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
@@ -10,6 +10,7 @@ ReactDOM.render(
     <GlobalStyles />
     <Content data-cy="content">
       <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">This is a work in progress</Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
