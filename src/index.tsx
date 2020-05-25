47acfb621ec92e8f294d29396cbcdaf88fbaf4db
jwkim <phoongdeng@gmail.com>
2020-05-26 02:43:42 +0900

    add grid component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
-import { Card, Content, Title } from './components'
+import { Card, Content, Grid, Title } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
@@ -10,7 +10,9 @@ ReactDOM.render(
     <GlobalStyles />
     <Content data-cy="content">
       <Title data-cy="title">Sudoku</Title>
-      <Card data-cy="card">This is a work in progress</Card>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
