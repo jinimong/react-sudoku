3fbfa3f823bbe9d1085221fdf05f31f54b610e9c
jwkim <phoongdeng@gmail.com>
2020-05-25 22:46:02 +0900

    apply style using theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: 'radial-gradient(#282c34CC, #282c34)',
+    black: '#282c34',
+    blue: '#a0e9fd',
+    lightBlud: '#caf3fe',
+    white: 'white',
+  },
+  transaction: '0.3s',
+}
