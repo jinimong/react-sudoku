77abce358378fab9fbc9f1c9b5219147939863c8
jwkim <phoongdeng@gmail.com>
2020-05-25 23:10:44 +0900

    add more styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
+import { Content, Title } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
