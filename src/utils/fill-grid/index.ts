5fa0bdeda108700d758a470940377366c743c328
jwkim <phoongdeng@gmail.com>
2020-05-28 01:48:52 +0900

    Add Fill-Grid with Unit Test

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,17 +1,41 @@
 import { GRID, NUMBERS } from 'typings'
-import { shuffle } from 'utils'
+import {
+  identifySquare,
+  shuffle,
+  isInRow,
+  isInCol,
+  isInSquare,
+  checkGrid,
+} from 'utils'
 
-const gridExample: GRID = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
-function fillGrid(grid: GRID) {}
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function fillGrid(grid: GRID) {
+  let row = 0
+  let col = 0
+  for (let i = 0; i < 81; ++i) {
+    row = Math.floor(i / 9)
+    col = i % 9
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers)
+
+      for (let value of numbers) {
+        if (!isInRow({ grid, row, value })) {
+          if (!isInCol({ grid, col, value })) {
+            const square = identifySquare({ grid, row, col })
+            if (!isInSquare({ square, value })) {
+              grid[row][col] = value
+              if (checkGrid(grid)) return true
+              else if (fillGrid(grid)) return true
+            }
+          }
+        }
+      }
+      break
+    }
+  }
+  grid[row][col] = 0
+}
 
 export default fillGrid
