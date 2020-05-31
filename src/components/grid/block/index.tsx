c3298fe6cf7124e89a7c93fbf53013fefacc69d2
jwkim <phoongdeng@gmail.com>
2020-06-01 04:17:38 +0900

    Add Number Button

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -12,15 +12,18 @@ interface IProps {
 
 interface IState {
   isActive: boolean
+  isPuzzle: boolean
   value: N
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
   const state = useSelector<IReducer, IState>(
-    ({ workingGrid, selectedBlock }) => ({
+    ({ challengeGrid, selectedBlock, workingGrid }) => ({
       isActive: selectedBlock
         ? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex
         : false,
+      isPuzzle:
+        challengeGrid && challengeGrid[rowIndex][colIndex] !== 0 ? true : false,
       value: workingGrid ? workingGrid[rowIndex][colIndex] : 0,
     })
   )
@@ -36,6 +39,7 @@ const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
       active={state.isActive}
       data-cy={`block-${rowIndex}-${colIndex}`}
       onClick={handleClick}
+      puzzle={state.isPuzzle}
     >
       {state.value === 0 ? '' : state.value}
     </Container>
