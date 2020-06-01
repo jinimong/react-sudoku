427b573f77201b374fddbb4de710eecc57626850
jwkim <phoongdeng@gmail.com>
2020-06-01 22:39:47 +0900

    Add build files

---
+++ build/precache-manifest.3fb40c7f58b916630915bca04ae7dc99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d956c12f1becdacef810e7e9de4c86bf",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "8add37f66e54da62b338",
+    "url": "/react-sudoku/static/js/2.dd66bad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-sudoku/static/js/2.dd66bad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972610f55dc365df8a1e",
+    "url": "/react-sudoku/static/js/main.c0d39b20.chunk.js"
+  },
+  {
+    "revision": "baed1a1e8bc181f8357a",
+    "url": "/react-sudoku/static/js/runtime-main.e7229eec.js"
+  }
+]);
