42a34be2f05ce9d7dea473989180f6478525e9f8
jwkim <phoongdeng@gmail.com>
2020-06-01 18:42:36 +0900

    Add New-Game

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
-import { Card, Content, Grid, Numbers, Title } from './components'
+import { Card, Content, Grid, NewButton, Numbers, Title } from './components'
 import { configureStore, unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
@@ -15,6 +15,7 @@ ReactDOM.render(
       <Content data-cy="content">
         <Title data-cy="title">Sudoku</Title>
         <Card data-cy="card">
+          <NewButton />
           <Grid />
           <Numbers />
         </Card>
